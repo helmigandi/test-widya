d9ec0416fb08c2a6e8328a49ccc252f0c16249f9
Helmi Sugandi <sugandihelmi@gmail.com>
2021-04-08 11:01:02 +0700

    add auth and user routes - auth will call authentication() function in /middlewares/auth - user will call getUserDataHandler() function with GET API method in controllers/UserController

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const UserController = require("./../controllers/UserController");
+const { authentication } = require("../middlewares/auth");
 
 router.get("/", (req, res) => {
   res.send("Wellcome to REST API Test, you must register and login first");
@@ -7,5 +8,7 @@ router.get("/", (req, res) => {
 
 router.post("/register", UserController.registerHandler);
 router.post("/login", UserController.loginHandler);
+router.use(authentication);
+router.get("/user", UserController.getUserDataHandler);
 
 module.exports = router;
