341ebc3f63a1456bc80cec4a39e8f1265fd3a46e
Helmi Sugandi <sugandihelmi@gmail.com>
2021-04-08 11:02:28 +0700

    add find by id function - function created for call mongodb to find user data with _id in users collection

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,4 +1,5 @@
 const { getDatabase } = require("./../config/mongodb");
+const { ObjectId } = require("mongodb");
 
 class UserModel {
   static async createUser(dataUser) {
@@ -10,6 +11,11 @@ class UserModel {
   static async findByEmail(email) {
     const databaseUsers = this.getDatabaseCollection();
     return databaseUsers.findOne({ email });
+  }
+
+  static async findById(_id) {
+    const databaseUsers = this.getDatabaseCollection();
+    return databaseUsers.findOne({ _id: ObjectId(_id) });
   }
 
   static getDatabaseCollection() {
