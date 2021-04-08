beddcfc57fc11a6eae393e075edbd165dd4aa3ba
Helmi Sugandi <sugandihelmi@gmail.com>
2021-04-08 13:32:43 +0700

    add mongodb atlas driver URI

--- config/mongodb.js
+++ config/mongodb.js
@@ -4,7 +4,7 @@ const { MongoClient } = require("mongodb");
 let database = null;
 
 // Connection URI
-const uri = "mongodb://localhost:27017";
+const uri = `mongodb://dbUser:dbUser@cluster0-shard-00-00.tfq7o.mongodb.net:27017,cluster0-shard-00-01.tfq7o.mongodb.net:27017,cluster0-shard-00-02.tfq7o.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-1437y7-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 // Create a new MongoClient
 const client = new MongoClient(uri, {
