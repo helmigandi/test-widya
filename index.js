c1d2a1c4c2a93476d88f04507db7db6208174c70
Helmi Sugandi <sugandihelmi@gmail.com>
2021-04-08 07:17:06 +0700

    initial commit - create express template - add json and urlencoded for Post API - add connect() function for check MongoDB connection

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { connect } = require("./config/mongodb");
+const app = express();
+const PORT = process.env.PORT || 3000;
+// const routes = require("./routes")
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+connect()
+  .then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(PORT, () => {
+      console.log(`REST API app listening at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Error");
+  });
