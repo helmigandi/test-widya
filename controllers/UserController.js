207a8dd68695eb81180f2d5a4ea6b49651f78303
Helmi Sugandi <sugandihelmi@gmail.com>
2021-04-08 09:48:16 +0700

    create login function - this function will check email and password null or not - this function will check if email in mongodb and same password with bcryptjs - if password match it will generate payload to add in JWT

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,6 @@
 const UserModel = require("./../models/UserModel");
 const { hashPassword, comparePassword } = require("../helpers/bcryptjs");
+const { generateToken } = require('../helpers/jwt')
 
 class UserController {
   static async registerHandler(req, res) {
@@ -16,6 +17,43 @@ class UserController {
       dataUser.password = hashPassword(dataUser.password);
       const response = await UserModel.createUser(dataUser);
       res.status(201).json(response.ops[0]);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: err.message });
+    }
+  }
+
+  static async loginHandler(req, res) {
+    const {email, password} = req.body
+    try {
+      if (!email || !password) {
+        throw new Error("email or password must be filled");
+      }
+
+      const dataUser = await UserModel.findByEmail(email);
+      
+      if (!dataUser) {
+        res.status(401).json({message: "Invalid email/password"});
+      }
+
+      const matchPassword = comparePassword(password, dataUser.password)
+
+      if(!matchPassword) {
+        res.status(401).json({message: "Invalid email/password"})
+      }
+
+      const payloadUser = {
+        _id: dataUser._id,
+        email: dataUser.email,
+        name: dataUser.name
+      }
+
+      const access_token = generateToken(payloadUser)
+
+      res.status(200).json({
+        access_token,
+        payloadUser
+      })
     } catch (err) {
       console.log(err);
       res.status(500).json({ message: err.message });
