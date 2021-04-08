9527a8aad8143898969062f33aae527331f52507
Helmi Sugandi <sugandihelmi@gmail.com>
2021-04-08 09:11:35 +0700

    create router - create welcome router with GET method - create register router with POST method and call registerHandler function from UserController class.

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const UserController = require("./../controllers/UserController");
+
+router.get("/", (req, res) => {
+  res.send("Wellcome to REST API Test, you must register and login first");
+});
+
+router.post("/register", UserController.registerHandler);
+
+module.exports = router;
